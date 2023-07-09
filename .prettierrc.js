00641c3f2a976facd3afffbf8945edcfd49f84c8
Bohdan Robota <newbusines123@gmail.com>
2023-07-09 22:45:36 +0300

    feat: Init project with prettier and eslint

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+jsxSingleQuote: false,
+printWidth: 100,
+semi: true,
+singleQuote: true,
+tabWidth: 2,
+useTabs: false
+}
---
+++ src/Home.tsx
@@ -0,0 +1,8 @@
+import React, { useContext } from 'react';
+import { Text } from 'react-native';
+
+const Home = () => {
+  return <Text>Home</Text>;
+};
+
+export default Home;
